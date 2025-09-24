555d7c496043785eadaf727a67f1437facd58395
HomHom1 <homahomen2@gmail.com>
2025-09-23 22:29:21 -0500

    added starter frontend code

---
+++ cookbook-app/src/app/sign-up/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function SignupPage() {
+    return (
+        <div className="flex flex-col gap-5 w-full h-full items-center">
+
+            <div>This is the sign up page</div>
+            <a className="border-1 w-fit px-3 py-2 rounded-xl" href="/">Home</a>
+            <a className="border-1 w-fit px-3 py-2 rounded-xl" href="/sign-in">Sign in</a>
+            <a className="border-1 w-fit px-3 py-2 rounded-xl" href="/sign-up">Sign up</a>
+
+        </div>
+    )
+}
