9092fff312eaa09dc6171edf7511c0833060a7de
fuzzyfish1 <zain@accucalc.com>
2025-10-02 01:35:58 -0500

    built an about segment on the home page

--- cookbook-app/src/app/page.tsx
+++ cookbook-app/src/app/page.tsx
@@ -6,11 +6,60 @@ export default function Home() {
 
       <ModeToggle />
 
+      {/* About Paragraph */}
+      <div className = "m-10 p-5">
+        <h1 className="text-center font-bold text-xl">About</h1>
+        <p className="text-center text-gray-150">
+          <br></br>
+          Have you ever realized that you are tired of eating out
+          but don't really know what to do with the ingredients you already have?
+          <br></br><br></br>
+          Do you "have food at home" that you don't know how to cook or takes to
+          long to cook so you end up eating out anyway?
+          <br></br><br></br>
+          We are Team Crunchy Tornado,
+          and this webapp was developed as part of a UIC CS442 project to solve
+          exactly this problem. Here on Team Crunchy Tornado, we are about that big
+          back food life, and can teach you to be as well.
+          <br></br><br></br>
+          Our application lets
+          users browse and discover meals to fit ingredients you already have. It is
+          a fun and interactive way to save money, and learn how to cook new meals at
+          the same time.
+          <br></br><br></br>
+        </p>
+
+        {/*
+            sideways Image bar
+            would be responsive to screen size and stack if I was goated
+        */}
+        <div className="flex flex-wrap gap-2">
+          <div className="flex-1 aspect-square">
+            <img
+              src = "/assets/beefyboi.jpg"
+              alt="Image 1"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="flex-1 aspect-square">
+            <img
+              src = "/assets/charcuterieBoard.jpg"
+              alt="Image 2"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+
+      </div>
+
+
       <div>This is the home page</div>
       <a className="border-1 w-fit px-3 py-2 rounded-xl" href="/">Home</a>
       <a className="border-1 w-fit px-3 py-2 rounded-xl" href="/sign-in">Sign in</a>
       <a className="border-1 w-fit px-3 py-2 rounded-xl" href="/sign-up">Sign up</a>
 
+
     </div>
   );
 }
+
