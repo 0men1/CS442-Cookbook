d912f24c15272e24ff685195b37b2cff50f694ae
tamziy <114886626+tamziy@users.noreply.github.com>
2025-10-22 23:37:00 -0500

    testing out backend

--- cookbook-app/src/app/profile/page.tsx
+++ cookbook-app/src/app/profile/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 
 
 export default function ProfilePage() {
+    const { data: session, status } = useSession();
+
+    if (status === "unauthenticated") return <div>You are not logged in.</div>
+    
     return (
         <div>
-            This is the profile page
+            <h1>Profile Page</h1>
+            <p>What's cooking, {session?.user?.username}!</p>
+            <p>Email {session?.user?.email}</p>
         </div>
     )
 }
