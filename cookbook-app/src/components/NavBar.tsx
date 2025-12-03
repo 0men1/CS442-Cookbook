8f2feacc36b90879e485f488a813990c55f6c338
tamziy <114886626+tamziy@users.noreply.github.com>
2025-12-03 13:19:42 -0600

    Remove Community tab as it is currently our home page tab

--- cookbook-app/src/components/NavBar.tsx
+++ cookbook-app/src/components/NavBar.tsx
@@ -13,7 +13,6 @@ export default function NavBar() {
   const pathname = usePathname();
 
   const tabs = [
-    { name: "Community", href: "/community" },
     { name: "Guides", href: "/guides" },
     { name: "Profile", href: "/profile" },
   ];
